167b88a41dbf1921eac2d40845920673d317bfdd
martin <martinleiro9@gmail.com>
2023-06-24 08:44:25 -0700

    pascalete

---
+++ pascal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+
+    if n <= 0:
+        return []
+    else:
+        triangle = [[1]]
+        for i in range(1, n):
+            row = [1]
+            for j in range(1, i):
+                num = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(num)
+            row.append(1)
+            triangle.append(row)
+        return triangle
+
+
+def main():
+    n = int(input("Ingrese el numero de filas para el triangulo de Pascal: "))
+    triangle = pascal_triangle(n)
+    print("Triángulo de Pascal:")
+    for row in triangle:
+        print(row)
+
+if __name__ == "__main__":
+    main()
