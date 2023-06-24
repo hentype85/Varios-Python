e918d6cc94609a6fd99b64a8c4901f768196a811
martin <martinleiro9@gmail.com>
2023-06-24 10:10:59 -0700

    romano

---
+++ romanoToInt.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if type(roman_string) != str:
+        return 0
+
+    d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    Rs = roman_string
+    res = 0
+
+    for i in range(len(Rs)):
+        if i == len(Rs) - 1 or d.get(Rs[i]) >= d.get(Rs[i + 1]):
+            res += d.get(Rs[i])
+        else:
+            res -= d.get(Rs[i])
+    return res
+
+
+result = roman_to_int("VII")
+print(result)
