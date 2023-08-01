200c61626f17b77e4e0966cc9d4b236513e80cf0
Martin <martinleiro9@gmail.com>
2023-08-01 12:35:23 -0700

    roman

--- romanoToInt.py
+++ romanoToInt.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_string):
-    if type(roman_string) != str:
+def roman_to_int(roman):
+    """convertir números romanos a enteros"""
+    if type(roman) != str:
         return 0
 
-    d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    Rs = roman_string
-    res = 0
+    d = {
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
+        'V': 5,
+        'I': 1
+    }
 
-    for i in range(len(Rs)):
-        if i == len(Rs) - 1 or d.get(Rs[i]) >= d.get(Rs[i + 1]):
-            res += d.get(Rs[i])
+    res = 0
+    for i in range(len(roman)):
+        if i == len(roman) - 1 or d.get(roman[i]) >= d.get(roman[i + 1]):
+            res += d.get(roman[i])
         else:
-            res -= d.get(Rs[i])
+            res -= d.get(roman[i])
     return res
 
 
-result = roman_to_int("VII")
-print(result)
+print(roman_to_int("VII"))
